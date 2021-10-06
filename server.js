840883945639aec8106677e63749a3ae2b38504c
David Currie <davidalexandercurrie@gmail.com>
2021-10-06 02:33:38 -0400

    str

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ io.on('connection', socket => {
         console.log(data);
       });
       let arr = tokenizer.tokenize(data);
-      const array = [...str.matchAll(/@\w+/)];
+      const array = [...data.matchAll(/@\w+/)];
       let newArr = array.concat(arr);
       io.to(socket.id).emit('event', newArr);
     } else {
