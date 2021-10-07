7dcc2251c92bb8cfc092d81ec1922140528c0bca
David Currie <davidalexandercurrie@gmail.com>
2021-10-06 20:57:16 -0400

    update

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const token = process.env.bearer_token;
 const endpointURLUserName = 'https://api.twitter.com/2/users/by/username/';
 const WordPOS = require('wordpos'),
   wordpos = new WordPOS();
+const containsEmoji = require('contains-emoji');
 
 app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -55,6 +56,36 @@ io.on('connection', socket => {
       });
       let newArr = data.split(/[\s.,!?":/]/g);
       let filteredArr = [];
+
+      function processData() {
+        let counts = {};
+        let keys = [];
+        for (let i = 0; i < filteredArr.length; i++) {
+          let word = filteredArr[i].toLowerCase();
+          if (
+            counts[word] === undefined &&
+            word !== '' &&
+            word !== 't' &&
+            word !== 'co'
+          ) {
+            counts[word] = 1;
+            keys.push(word);
+          } else {
+            counts[word] = counts[word] + 1;
+          }
+        }
+        keys.sort(compare);
+
+        function compare(a, b) {
+          var countA = counts[a];
+          var countB = counts[b];
+          return countB - countA;
+        }
+
+        let dataToSend = { keys, counts };
+
+        io.to(socket.id).emit('event', dataToSend);
+      }
       // let data = [[noun, adjective, mention, emoji], text];
       if (settings.includes(true)) {
         wordpos.getPOS(newArr.join(' '), result => {
@@ -63,49 +94,25 @@ io.on('connection', socket => {
             console.log(filteredArr);
           }
           if (settings[1]) {
+            filteredArr = filteredArr.concat(result.adjectives);
             console.log('adjective');
           }
           if (settings[2]) {
+            filteredArr = filteredArr.concat(newArr.join(' ').match(/@w+/g));
             console.log('mention');
           }
           if (settings[3]) {
             console.log('emoji');
-          }
-          if (!settings.includes(true)) {
-            console.log('nothing');
+            newArr.forEach(element => {
+              if (containsEmoji(element)) {
+                filteredArr.push(element);
+              }
+            });
           }
         });
-
-        function processData() {
-          let counts = {};
-          let keys = [];
-          for (let i = 0; i < filteredArr.length; i++) {
-            let word = filteredArr[i].toLowerCase();
-            if (
-              counts[word] === undefined &&
-              word !== '' &&
-              word !== 't' &&
-              word !== 'co'
-            ) {
-              counts[word] = 1;
-              keys.push(word);
-            } else {
-              counts[word] = counts[word] + 1;
-            }
-          }
-          keys.sort(compare);
-
-          function compare(a, b) {
-            var countA = counts[a];
-            var countB = counts[b];
-            return countB - countA;
-          }
-
-          let dataToSend = { keys, counts };
-
-          io.to(socket.id).emit('event', dataToSend);
-        }
+        processData();
       } else {
+        processData();
         console.log('no params');
       }
     } else {
